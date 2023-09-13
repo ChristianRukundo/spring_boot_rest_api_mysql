1dfd258ef9617d51cd605e84d72edd4aca469904
ChristianRukundo <siboruremarukundoc1@gmail.com>
2023-09-13 22:08:41 +0200

    worked on Student model

--- src/main/java/com/christian/mysql_rest_api/model/Student.java
+++ src/main/java/com/christian/mysql_rest_api/model/Student.java
@@ -1,5 +1,66 @@
 package com.christian.mysql_rest_api.model;
 
-public class Student {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "class")
+    private String classroom;
+
+    @Column(name = "passed")
+    private boolean passed;
+
+    public Student() {}
+
+    public Student(String name, String classroom, boolean passed) {
+        this.name = name;
+        this.classroom = classroom;
+        this.passed = passed;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", classroom='" + classroom + '\'' +
+                ", passed=" + passed +
+                '}';
+    }
 }
